7b1f4f6b9b9944f734c3c839236c3dfb82325acb
Ahmet Sezer <ahmetsezerakdeniz19@gmail.com>
2023-02-03 17:41:15 +0300

    Database connection established

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//Connect to DB
+mongoose.set('strictQuery', true);
+mongoose.connect(
+    "mongodb://127.0.0.1/pcat-test-db",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/');
 });
 
 const port = 3000;
