4bf57ac163f65dc5480dcc1f9ef26501c5496444
Ahmet Sezer <ahmetsezerakdeniz19@gmail.com>
2023-02-05 16:28:03 +0300

    CRUD and MVC arrangements done.

--- app.js
+++ app.js
@@ -1,48 +1,44 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const methodOverride = require('method-override');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
-const path = require('path');
-const Photo = require('./models/Photo');
+const photoController = require('./controllers/photoController');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
 //Connect to DB
 mongoose.set('strictQuery', true);
-mongoose.connect(
-    "mongodb://127.0.0.1/pcat-test-db",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-)
+mongoose.connect('mongodb://127.0.0.1/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
-  res.render('index', {
-    photos
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add', (req, res) => {
-  res.render('add');
-});
+app.get('/', photoController.getAllPhotos);
+app.post('/photos', photoController.createPhoto);
+app.get('/photos/:id', photoController.getPhoto);
+app.put('/photos/:id', photoController.uptadePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)
-  res.redirect('/');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 app.listen(port, () => {
